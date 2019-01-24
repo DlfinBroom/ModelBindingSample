7516c60aa2b96a4b23dec25f7dee055afc9e61a1
lampert309 <lampert309@gmail.com>
2019-01-24 08:55:25 -0800

    Add project files.

---
+++ ModelBindingSample/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelBindingSample.Models;
+
+namespace ModelBindingSample.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult About() {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact() {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [HttpGet] // Default
+        public IActionResult StudentForm() {
+            return View();
+        }
+
+        [HttpPost] // Used when Submited/Posted to the server
+        public IActionResult StudentForm(IFormCollection data) {
+            string FirstName = data["FirstName"];
+            string LastName = data["LastName"];
+
+            // ToDo: Add Validation
+
+            Student stu = new Student();
+            stu.FirstName = FirstName;
+            stu.LastName = LastName;
+
+            // ToDo: Add Student to Db
+
+            // ViewData is automatically visable in the view
+            ViewData["Msg"] = "Thanks for adding " + FirstName + " " + LastName;
+
+            return View();
+        }
+
+        [HttpGet] // Default
+        public IActionResult StudentBinding() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult StudentBinding(Student stu) {
+            // Validate input
+            if (ModelState.IsValid) {
+                // Add to database
+
+                return RedirectToAction("Index");
+            }
+            return View(stu);
+            
+        }
+
+        public IActionResult StudentAcaffold() {
+            return View();
+        }
+    }
+}
---
+++ ModelBindingSample/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBindingSample.Models {
+    public class Student {
+        [Display(Name = "First Name:")]
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Must be between 3 and 30 charactors.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name:")]
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Must be between 3 and 30 charactors.")]
+        public string LastName { get; set; }
+    }
+}
